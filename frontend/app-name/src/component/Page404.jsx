79105eb3fd7b1d95ecf47cb74caf736d5ace4009
B02_Al_Sous <khaledalsous.ahmed.com>
2025-12-30 02:27:06 +0300

    page404edit

--- frontend/app-name/src/component/Page404.jsx
+++ frontend/app-name/src/component/Page404.jsx
@@ -6,7 +6,6 @@ import "./page404.css";
 const Page404 = () => {
   return (
     <>
-      <Navbar />
       <div className="page404">
         <h1 className="page404__title">404</h1>
         <h2 className="page404__subtitle">Page Not Found</h2>
