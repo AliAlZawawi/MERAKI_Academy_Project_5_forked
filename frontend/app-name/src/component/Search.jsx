933df0de17f5a8b9fe48d139c34a552367b3ff04
AliAlzaawawi <alzawawe.ali@gmail.com>
2026-01-04 17:24:48 +0300

    search

--- frontend/app-name/src/component/Search.jsx
+++ frontend/app-name/src/component/Search.jsx
@@ -4,18 +4,12 @@ import {setCourses} from "../redux/coursesSlice"
 import { useNavigate } from "react-router-dom";
 import ListGroup from 'react-bootstrap/ListGroup';
 import "./Search.css"
-const courses = [
-  { id: 1, title: "Web Development" },
-  { id: 2, title: "UI / UX Design" },
-  { id: 3, title: "Backend Development" },
-  { id: 4, title: "Mobile App Development" },
-  { id: 5, title: "Data Structures & Algorithms" },
-];
+import { setCourseId } from "../redux/courseDetailsSlice";
 
 const Search = () => {
   const navigate = useNavigate();
  const dispatch = useDispatch();
-  //const courses = useSelector((state) => state.courses.courses);
+  const courses = useSelector((state) => state.courses.courses);
   const [searchValue, setSearchValue] = useState("");
   const [searchCourses, setSearchCourses] = useState([]);
 
@@ -47,7 +41,9 @@ const Search = () => {
         <ListGroup className="search-results">
            {searchCourses.map(course => (
       <ListGroup.Item  key={course.id}
-              onClick={() => navigate(`/courseDetails/${course.id}`)}>  {course.title}</ListGroup.Item>
+              onClick={() => {
+                 dispatch(setCourseId(setCourseId(course.id)))
+                navigate(`/courseDetails`)}}>  {course.title}</ListGroup.Item>
     ))}
     </ListGroup>
  )}
